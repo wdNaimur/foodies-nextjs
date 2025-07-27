79794af3bd4c27d384d9fc6590770f05f70ba5ca
NaimurWD <naimur.wd@gmail.com>
2025-07-27 22:04:24 +0600

    data fetching, suspense learned.

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { Suspense } from "react";
+import classes from "./page.module.css";
+import Link from "next/link";
+import MealsGrid from "../_components/meals/meals-grid";
+import { getMeals } from "../_lib/meals";
+
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
 
 export default function MealsPage() {
   return (
-    <main>
-      <h1>This will be mealsPage</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun.
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching Meals...</p>}
+        >
+          <Meals />
+        </Suspense>
+      </main>
+    </>
   );
 }
